56b50792a5e87a25eb99bdc91c02e11612195522
janner <janner@mit.edu>
2019-10-01 00:10:40 -0700

    fixed walker2d config file

--- examples/config/walker2d/0.py
+++ examples/config/walker2d/0.py
@@ -10,7 +10,7 @@ params = {
     'kwargs': {
         'epoch_length': 1000,
         'train_every_n_steps': 1,
-        'n_train_repeat': 2, #20,
+        'n_train_repeat': 20,
         'eval_render_mode': None,
         'eval_n_episodes': 1,
         'eval_deterministic': True,
@@ -18,23 +18,16 @@ params = {
         'discount': 0.99,
         'tau': 5e-3,
         'reward_scale': 1.0,
-        ####
-        # 'model_reset_freq': 1000,
-        'model_train_freq': 250, # 250
-        # 'retain_model_epochs': 2,
-        'model_pool_size': 2e6,
-        'rollout_batch': 100e3, # 40e3
-        'rollout_length': 1,
+
+        'model_train_freq': 250,
+        'model_retain_epochs': 1,
+        'rollout_batch_size': 100e3,
         'deterministic': False,
         'num_networks': 7,
         'num_elites': 5,
         'real_ratio': 0.05,
-        # 'entropy_mult': 0.5,
         'target_entropy': -3,
         'max_model_t': None,
-        # 'max_dev': 0.25, 
-        # 'marker': 'early-stop_10rep_stochastic',
-        'rollout_length_params': [20, 150, 1, 1], ## epoch, loss, length
-        # 'marker': 'dump',
+        'rollout_schedule': [20, 150, 1, 1],
     }
 }
