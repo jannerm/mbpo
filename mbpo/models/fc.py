ac694ff9f1ebb789cc5b3f164d9d67f93ed8f129
Michael Janner <janner@berkeley.edu>
2021-03-21 22:22:59 -0700

    removed redundant `set_model_vars` definition

--- mbpo/models/fc.py
+++ mbpo/models/fc.py
@@ -57,20 +57,9 @@ class FC:
         bias = biases[idx].copy()
         return {'weights': weight, 'biases': bias}
 
-    def set_model_vars(self, idx, sess, variables):
-        for attr, var in variables.items():
-            tensor = getattr(self, attr)
-            op = tensor[idx].assign(var)
-            sess.run(op)
-            # print('assigned {}: {}'.format(attr, idx))
-
     def set_model_vars(self, variables):
         ops = [getattr(self, attr).assign(var) for attr, var in variables.items()]
         return ops
-        # for attr, var in variables.items():
-            # tensor = getattr(self, attr)
-            # op = tensor.assign(var)
-
 
     def reset(self, sess):
         sess.run(self.weights.initializer)
